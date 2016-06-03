bffe2738ef22ecdb0d2f1d076a9540969ad35c7c
R.SIRHC <christopher.r99@live.com>
2016-06-03 16:38:59 -0400

    Innitial Commit

--- Hero Assignment/Program.cs
+++ Hero Assignment/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Christopher Ritchil 
+ * Student # : 300702644
+ * Date Modified: June 1, 2016
+ * Description: This program demonstrate COMP123 S2016 Assignment #1 requirements
+ * Version: 0.1.0
+ */
+
 namespace Hero_Assignment
 {
+    /**
+     * 
+     * This class is driver class for Program
+     * 
+     * @class Program
+     */
     class Program
-    {
+    {   /**
+         * 
+         * this main method for driver class Program
+         * 
+         * @method Main
+         * @param {string[]} args
+         */
         static void Main(string[] args)
         {
+            // create instance of the Hero Class
+            Hero HiroNakamura = new Hero("Hiro Nakamura");
+            HiroNakamura.Show();
+            HiroNakamura.Fight();
         }
+
     }
 }
